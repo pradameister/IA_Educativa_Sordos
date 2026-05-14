1d001e875bbe97ba344505c0a7763bbc0401bd6e
JHON PRADA <40615483+pradameister@users.noreply.github.com>
2026-05-13 20:09:50 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
     port: Number(process.env.PORT) || 5173,
   },
   preview: {
-  host: '0.0.0.0',
-  port: Number(process.env.PORT) || 5173,
-  allowedHosts: true,
-},
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: true,
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
