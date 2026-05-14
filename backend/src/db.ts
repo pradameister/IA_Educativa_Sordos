9acd4e3362086adc7fddadf6b89cc466d648b60c
manununuel63-web <manununuel63@gmail.com>
2026-05-13 21:35:27 -0500

    Update db.ts
    pruebas

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectDB() {
-  const MONGO_URI = process.env.DATABASE_URL;
+  const MONGO_URI = process.env.MONGODB_URI || process.env.DATABASE_URL;
 
   if (!MONGO_URI) {
     console.error('❌ Error: DATABASE_URL no está definida en las variables de entorno.');
