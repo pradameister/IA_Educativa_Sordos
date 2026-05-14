91f4641aa378905eaf6bf3d17ba9dde859d05441
JHON PRADA <40615483+pradameister@users.noreply.github.com>
2026-05-13 20:06:21 -0500

    Update vite.config.ts
    vista

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
     port: Number(process.env.PORT) || 5173,
   },
   preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 5173,
-    allowedHosts: ['frontend-production-09d0.up.railway.app'],
-  },
+  host: '0.0.0.0',
+  port: Number(process.env.PORT) || 5173,
+  allowedHosts: true,
+},
   build: {
     outDir: 'dist',
     sourcemap: true,
